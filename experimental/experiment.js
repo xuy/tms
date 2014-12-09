e1236af330d8059a2aefe06c93d03fbaa83ac0bc
Eric Xu <xu.mathena@gmail.com>
2014-12-09 14:43:07 -0800

    support better result rendering

---
+++ experimental/experiment.js
@@ -0,0 +1,293 @@
+var j = {
+    "serial-number": "78925764",
+    "registration-number": "3289253",
+    "transaction-date": "20140411",
+    "case-file-header": {
+        "filing-date": "20060710",
+        "registration-date": "20070904",
+        "status-code": "710",
+        "status-date": "20140411",
+        "mark-identification": "AMERICAN SECCO",
+        "mark-drawing-code": "4000",
+        "amend-to-register-date": "20060809",
+        "cancellation-code": "2",
+        "cancellation-date": "20140411",
+        "domestic-representative-name": "MARK HARRISON",
+        "attorney-docket-number": "31915-233344",
+        "attorney-name": "MARK HARRISON",
+        "principal-register-amended-in": "F",
+        "supplemental-register-amended-in": "T",
+        "trademark-in": "T",
+        "collective-trademark-in": "F",
+        "service-mark-in": "F",
+        "collective-service-mark-in": "F",
+        "collective-membership-mark-in": "F",
+        "certification-mark-in": "F",
+        "cancellation-pending-in": "F",
+        "published-concurrent-in": "F",
+        "concurrent-use-in": "F",
+        "concurrent-use-proceeding-in": "F",
+        "interference-pending-in": "F",
+        "opposition-pending-in": "F",
+        "section-12c-in": "F",
+        "section-2f-in": "F",
+        "section-2f-in-part-in": "F",
+        "renewal-filed-in": "F",
+        "section-8-filed-in": "F",
+        "section-8-partial-accept-in": "F",
+        "section-8-accepted-in": "F",
+        "section-15-acknowledged-in": "F",
+        "section-15-filed-in": "F",
+        "supplemental-register-in": "T",
+        "foreign-priority-in": "F",
+        "change-registration-in": "F",
+        "intent-to-use-in": "T",
+        "intent-to-use-current-in": "F",
+        "filed-as-use-application-in": "F",
+        "amended-to-use-application-in": "F",
+        "use-application-currently-in": "F",
+        "amended-to-itu-application-in": "F",
+        "filing-basis-filed-as-44d-in": "F",
+        "amended-to-44d-application-in": "F",
+        "filing-basis-current-44d-in": "F",
+        "filing-basis-filed-as-44e-in": "F",
+        "filing-basis-current-44e-in": "T",
+        "amended-to-44e-application-in": "F",
+        "without-basis-currently-in": "F",
+        "filing-current-no-basis-in": "F",
+        "color-drawing-filed-in": "F",
+        "color-drawing-current-in": "F",
+        "drawing-3d-filed-in": "F",
+        "drawing-3d-current-in": "F",
+        "standard-characters-claimed-in": "T",
+        "filing-basis-filed-as-66a-in": "F",
+        "filing-basis-current-66a-in": "F",
+        "law-office-assigned-location-code": "M10",
+        "current-location": "PUBLICATION AND ISSUE SECTION",
+        "location-date": "20070904",
+        "employee-name": "FINE, STEVEN R"
+    },
+    "case-file-statements": {
+        "case-file-statement": [
+            {
+                "type-code": "D10000",
+                "text": "\"SECCO\""
+            },
+            {
+                "type-code": "GS0331",
+                "text": "Alcoholic beverages, namely wines, sparkling wines and semi-sparkling wines"
+            },
+            {
+                "type-code": "TR0010",
+                "text": "The term SECCO in the mark translates into English as DRY."
+            }
+        ]
+    },
+    "case-file-event-statements": {
+        "case-file-event-statement": [
+            {
+                "code": "C8..",
+                "type": "O",
+                "description-text": "CANCELLED SEC. 8 (6-YR)",
+                "date": "20140411",
+                "number": "14"
+            },
+            {
+                "code": "ASGN",
+                "type": "I",
+                "description-text": "AUTOMATIC UPDATE OF ASSIGNMENT OF OWNERSHIP",
+                "date": "20091118",
+                "number": "13"
+            },
+            {
+                "code": "ASGN",
+                "type": "I",
+                "description-text": "AUTOMATIC UPDATE OF ASSIGNMENT OF OWNERSHIP",
+                "date": "20091111",
+                "number": "12"
+            },
+            {
+                "code": "R.SR",
+                "type": "A",
+                "description-text": "REGISTERED-SUPPLEMENTAL REGISTER",
+                "date": "20070904",
+                "number": "11"
+            },
+            {
+                "code": "PREV",
+                "type": "O",
+                "description-text": "LAW OFFICE PUBLICATION REVIEW COMPLETED",
+                "date": "20070730",
+                "number": "10"
+            },
+            {
+                "code": "CNTA",
+                "type": "O",
+                "description-text": "APPROVED FOR REGISTRATION SUPPLEMENTAL REGISTER",
+                "date": "20070730",
+                "number": "9"
+            },
+            {
+                "code": "RCCK",
+                "type": "S",
+                "description-text": "LIE CHECKED SUSP - TO ATTY FOR ACTION",
+                "date": "20070613",
+                "number": "8"
+            },
+            {
+                "code": "ALIE",
+                "type": "A",
+                "description-text": "ASSIGNED TO LIE",
+                "date": "20070613",
+                "number": "7"
+            },
+            {
+                "code": "CNSL",
+                "type": "F",
+                "description-text": "LETTER OF SUSPENSION MAILED",
+                "date": "20061212",
+                "number": "6"
+            },
+            {
+                "code": "CNSL",
+                "type": "R",
+                "description-text": "SUSPENSION LETTER WRITTEN",
+                "date": "20061211",
+                "number": "5"
+            },
+            {
+                "code": "DOCK",
+                "type": "D",
+                "description-text": "ASSIGNED TO EXAMINER",
+                "date": "20061208",
+                "number": "4"
+            },
+            {
+                "code": "AMPX",
+                "type": "O",
+                "description-text": "APPLICANT AMENDMENT PRIOR TO EXAMINATION - ENTERED",
+                "date": "20060829",
+                "number": "3"
+            },
+            {
+                "code": "MAIL",
+                "type": "I",
+                "description-text": "PAPER RECEIVED",
+                "date": "20060829",
+                "number": "2"
+            },
+            {
+                "code": "NWAP",
+                "type": "I",
+                "description-text": "NEW APPLICATION ENTERED IN TRAM",
+                "date": "20060713",
+                "number": "1"
+            }
+        ]
+    },
+    "foreign-applications": {
+        "foreign-application": {
+            "registration-date": "20041206",
+            "registration-expiration-date": "20141031",
+            "entry-number": "1",
+            "country": "DE",
+            "registration-number": "300457792",
+            "foreign-priority-claim-in": "F"
+        }
+    },
+    "classifications": {
+        "classification": {
+            "international-code-total-no": "1",
+            "us-code-total-no": "2",
+            "international-code": "033",
+            "us-code": [
+                "047",
+                "049"
+            ],
+            "status-code": "2",
+            "status-date": "20140411",
+            "first-use-anywhere-date": "0",
+            "first-use-in-commerce-date": "0",
+            "primary-code": "033"
+        }
+    },
+    "correspondent": {
+        "address-1": "MARK HARRISON",
+        "address-2": "VENABLE LLP",
+        "address-3": "575 7TH ST NW",
+        "address-4": "WASHINGTON, DC 20004-1607"
+    },
+    "case-file-owners": {
+        "case-file-owner": [
+            {
+                "entry-number": "1",
+                "party-type": "32",
+                "nationality": {
+                    "country": "DE"
+                },
+                "legal-entity-type-code": "13",
+                "party-name": "HENKELL & CO. SEKTKELLEREI KG",
+                "address-1": "BIEBRICHER ALLEE 142",
+                "city": "WIESBADEN",
+                "country": "DE",
+                "postcode": "65187"
+            },
+            {
+                "entry-number": "1",
+                "party-type": "31",
+                "nationality": {
+                    "country": "DE"
+                },
+                "legal-entity-type-code": "13",
+                "party-name": "HENKELL & CO. SEKTKELLEREI KG",
+                "address-1": "BIEBRICHER ALLEE 142",
+                "city": "WIESBADEN",
+                "country": "DE",
+                "postcode": "65187"
+            },
+            {
+                "entry-number": "1",
+                "party-type": "30",
+                "nationality": {
+                    "country": "DE"
+                },
+                "legal-entity-type-code": "13",
+                "party-name": "Henkell & Söhnlein Sektkellereien KG",
+                "address-1": "Biebricher Allee 142",
+                "city": "Wiesbaden",
+                "country": "DE",
+                "postcode": "D-65187"
+            },
+            {
+                "entry-number": "1",
+                "party-type": "10",
+                "nationality": {
+                    "country": "DE"
+                },
+                "legal-entity-type-code": "13",
+                "party-name": "Henkell & Söhnlein Sektkellereien KG",
+                "address-1": "Biebricher Allee 142",
+                "city": "Wiesbaden",
+                "country": "DE",
+                "postcode": "D-65187"
+            }
+        ]
+    }
+}
+
+function GetKeyValueRow(table, data, field, better_name) {
+	AddRow(table, better_name, data[field])
+}
+
+function AddRow(table, key, value) {
+	row = $("<tr>");
+	col1 = $("<td>");
+	col1.text(key);
+	col2 = $("<td>");
+	col2.text(value)
+	row.append(col1);
+	row.append(col2);
+	table.append(row);
+}
+
+console.log(j['serial-number'])
--- frontend/js/display.js
+++ frontend/js/display.js
@@ -1,3 +1,44 @@
-<script>
+function GetKeyValueRow(table, data, field, better_name) {
+	if (data.hasOwnProperty(field)) {
+		AddRow(table, better_name, data[field])
+	}
+}
 
-</script>
+function AddRow(table, key, value) {
+	row = $("<tr>");
+	col1 = $("<td>");
+	col1.text(key);
+	col2 = $("<td>");
+	col2.text(value)
+	row.append(col1);
+	row.append(col2);
+	table.append(row);
+}
+
+function processResultController($scope) {
+	result = $scope.result;
+	var table = angular.element('table')
+
+
+	GetKeyValueRow(table, result, 'serial-number', 'Serial Number');
+    
+    case_header = result['case-file-header'];
+    GetKeyValueRow(table, case_header, 'mark-identification', 'Word Mark');
+    GetKeyValueRow(table, case_header, 'domestic-representative-name', 'Attorney of Record');
+    
+    statements = result['case-file-statements']
+    console.log(statements);
+
+    text = ''
+    if (statements.hasOwnProperty('case-file-statement')) {
+        statement = statements['case-file-statement'];
+        console.log(statement)
+        for (var i = 0; i < statement.length; i++) {
+    	    text += statement[i]['text'];
+            text +='\n'
+        }
+    }
+    if (text != '') {
+	    AddRow(table, "Trademark Text", text)
+    }
+}
